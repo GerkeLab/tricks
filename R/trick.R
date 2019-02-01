cfd850bf6bba83f1688347ebd4791c84b05e7033
Travis Gerke <tgerke@hsph.harvard.edu>
2019-01-31 21:15:40 -0500

    create a skeleton of trick.R

--- R/trick.R
+++ R/trick.R
@@ -1,3 +1,20 @@
 trick <- function(dat = iris) {
+  # get all pairwise combinations of columns
+  combs <- combn(names(dat), 2)
 
+  # initialize p value
+  p <- 1
+  for (i in 1:ncol(combs)) {
+    x <- combs[1,i]
+    y <- combs[2,i]
+
+    # store class of x and y
+
+    # compute a p-value based on x/y classes
+
+    if(p < .05) {
+      # out <- the formula that led to the significant result
+      break
+    }
+  }
 }
